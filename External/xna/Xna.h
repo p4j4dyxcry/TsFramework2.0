8dfdb48c9cc33158a4a524cfa8bbc5e164a0939c
Yuki Tsuneyama <yuki.tsuneyama@siliconstudio.co.jp>
2017-10-31 11:43:12 +0900

    【追加】XNAMathの追加

---
+++ External/xna/Xna.h
@@ -0,0 +1,16 @@
+//!*******************************************************
+//!	Xna.h
+//!
+//!	XnaMathを使用可能にするヘッダ
+//!
+//!	著作権	2016 Yuki Tsuneyama
+//!	作成日時	16.01.13
+#pragma once
+#include <Windows.h>
+
+//! xna のインクルード
+#include "xnamath.h"
+#include "xnamathconvert.inl"
+#include "xnamathmatrix.inl"
+#include "xnamathmisc.inl"
+#include "xnamathvector.inl"
